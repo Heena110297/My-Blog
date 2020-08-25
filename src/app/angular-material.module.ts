17f016a543fd24912c72aed99e90b65368cf2b8a
Heena110297 <heena.mittal@nagarro.com>
2020-08-25 22:53:31 +0530

    Initial Commit

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+@NgModule({
+declarations: [],
+exports: [
+MatTabsModule
+],
+providers: []
+})
+
+export class AngularMaterialModule{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AngularMaterialModule} from './angular-material.module';
+import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
