2d51e5d427e5721b6310ee19498fea8e687ac5b5
Heena110297 <heena.mittal@nagarro.com>
2020-08-25 23:49:03 +0530

    added posts component

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
 @NgModule({
 declarations: [],
 exports: [
-MatTabsModule
+MatTabsModule,
+MatFormFieldModule,
+MatInputModule,
+MatButtonModule
 ],
 providers: []
 })
