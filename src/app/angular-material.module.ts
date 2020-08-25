9defb0615640a9771779848d668edc8821776f84
Heena110297 <heena.mittal@nagarro.com>
2020-08-26 00:33:54 +0530

    added button

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
